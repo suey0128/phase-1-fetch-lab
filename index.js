b7b8aa019069f6ce30db538f212184a11b9049ab
suey0128 <syui0128@gmail.com>
2021-05-26 13:57:24 -0700

    commit tree 7517c21175d56c152c8b2e1917ed3fdadbcdfd90

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+        // console.log(json);
+
+
+
+
+
+
+
+  .catch(error => console.error('Error:', error))
 }
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +29,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+//For practice 
+
+// console.log(json['4']); // the 5th book
+    
+// json.forEach(SecLevelElem => {
+//   // console.log(SecLevelElem);
+//   // console.log(Object.values(SecLevelElem.characters));
+//   let xxx = Object.values(SecLevelElem.characters);
+//   let count = 1030;
+//   if (xxx.length < count) {
+//     count = count - xxx.length
+//   } else {
+//     console.log(xxx[count]);
+//   }    
+// }) // The 1031st character in the series 
+
+// let totalPage = 0;  
+// json.forEach(SecLevelElem => { 
+//   totalPage += SecLevelElem.numberOfPages;
+// }) 
+// console.log(totalPage); //The total number of pages of all the books
